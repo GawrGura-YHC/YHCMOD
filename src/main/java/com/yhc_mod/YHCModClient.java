73893858f416a29a7bb2899ba9d8e62bdfb3685a
yuhaochen <3379444580@qq.com>
2023-06-22 18:41:15 +0800

    Initial commit

---
+++ src/main/java/com/yhc_mod/YHCModClient.java
@@ -0,0 +1,10 @@
+package com.yhc_mod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class YHCModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/yhc_mod/YHCModDataGenerator.java
@@ -0,0 +1,11 @@
+package com.yhc_mod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class YHCModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
